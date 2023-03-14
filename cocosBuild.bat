3c62469a6fbc0d4abbcfe2ad3fad837118c5eafc
wang <169281138@qq.com>
2023-03-14 18:03:58 +0800

    命令行打包

---
+++ cocosBuild.bat
@@ -0,0 +1,7 @@
+@REM set cocos = "D:\CocosDashboard\resources\.editors\Creator\3.6.3\CocosCreator.exe"
+@REM set path = "D:\CocosGame\Demo3.6"
+
+@REM %cocos% --path %path% --build "platform=web-mobile;debug=false"
+
+@REM echo "build over"
+D:\CocosDashboard\resources\.editors\Creator\3.6.3\CocosCreator.exe --project D:\CocosGame\Demo3.6 --build configPath=D:\CocosGame\Demo3.6\build/buildConfig_web-mobile.json
